bb88e8a8d747308fd5f841ffb1374d1ffcc89471
Labhesh Ajay Sonawane <lasonawane50@gmail.com>
2025-12-16 17:07:23 +0530

    Initial commit

---
+++ 001_CH1_Basics.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("\nWhat is your name?")
+my_name = input('>') # Ask for thier name
+print("\nIt is good to meet you, " + my_name)
+print("Length of your name is : ",end="")
+print(len(my_name)) # Ask for thier age
+my_age = input("\nWhat is your age? \n>")
+print("You will be "+ str(int(my_age) + 1)+" in a year")
